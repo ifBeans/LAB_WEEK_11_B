3a6699fa87540addc1e3081879ffdec898f5b0c6
ifBeans <fabiandustin27@gmail.com>
2025-11-24 23:36:26 +0700

    Commit No. 2: Photo and Video using FileProvider and MediaStore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,8 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation("commons-io:commons-io:2.6")
+    implementation(libs.androidx.material3)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
